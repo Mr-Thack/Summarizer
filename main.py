3013c71890c0dc773e63451f3abc582ae06108c5
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2024-11-07 09:42:16 -0500

    Added a description field to help the AI with summarizing the enhancement requests using the Tools API of ChatGPT

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import argparse
 import yaml
 import pandas as pd
 import os
+from pydantic import BaseModel
+from dataclasses import dataclass
 
 # This section is for flags
 DEBUG = None
@@ -43,7 +45,8 @@ class DictObject(object):
     def from_dict(cls, d):
         return json.loads(json.dumps(d), object_hook=DictObject)
 
-def prompt(system_prompt, question):
+
+def prompt(system_prompt, question, response_model=None):
     args = {
         'model': MODEL,
         'messages': [
@@ -57,13 +60,24 @@ def prompt(system_prompt, question):
             }
         ]
     }
+    if response_model and 'ollama' not in sys.modules:
+        # args['temperature'] = 0
+        args['response_format'] = response_model 
     response = None
     if 'ollama' in sys.modules:
         response = DictObject.from_dict(CLIENT.chat(**args))
     else:
-        completion = CLIENT.chat.completions.create(**args)
+        completion = None
+        if response_model:
+            completion = CLIENT.beta.chat.completions.parse
+        else:
+            completion = CLIENT.chat.completions.chat
+        completion = completion(**args)
         response = completion.choices[0]
-    return response.message.content
+    if response_model:
+        return response.message.parsed
+    else:
+        return response.message.content
 
 def write(data, filename):
     with open(filename, 'w') as f:
@@ -131,10 +145,14 @@ def summarize_req(r):
     dpr(res, '\n')
     return res
 
+class Topic(BaseModel):
+    name: str
+    description: str
+
 def sort_req(summary, commonalities):
     dpr(summary)
     mprompt = prompt_sort(commonalities) # Generates Prompt
-    res = prompt(mprompt, summary)
+    res = prompt(mprompt, summary, Topic)
     dpr(res, "\n")
     return res
 
@@ -214,57 +232,65 @@ def summarize_file(f):
 def summarize(targets):
     return [summarize_file(t) for t in targets]
 
-def sort_file(f):
-    # This will hold an object like this:
-    # {
+def sort_file(f): 
     #   Subject Category Name 1: {
-    #       Categorization Name 1: [req ids],
-    #       Categorization Name 2: [req ids],
+    #       Categorization Name 1: {
+    #           desc: Description,
+    #           requests: [req ids]
+    #       },
+    #       ...
     #   },
     #   Subject Category Name 2: ...
     # }
-    commonalities = {}
+    subjects = dict() # Holds requests corresponding to what topic to what category 
     for i, data in enumerate(SortReader(f)):
         # First, find get the index (The Subject Name)
         index = data['request']['Category']
 
         # dict.get() will return [] if that entry is null
-        subject = commonalities.get(index, {})
+        subject = subjects.get(index, {})
         dpr("Subject: {}".format(index))
         # Then get a category for the request
-        category = sort_req(data['summary'], subject)
-      
+        topic: Topic = sort_req(data['summary'], subject)
+        if topic is None:
+            pass # Something went wrong, I don't care enough to debug 
+
+
         # if request_category in current request_categories of subject_category
-        if category in subject.keys():
+        if topic.name in subject.keys():
             # Just append the current req id
-            subject[category].append(i)
+            subject[topic.name]["requests"].append(i)
         else:
             # Make a list only containing the current req id
-            subject[category] = [i]
+            subject[topic.name] = {
+                "description": topic.description,
+                "requests": [i]
+            }
 
         # Then put the modified subject category back into the big list
-        commonalities[index] = subject
-        write(commonalities, get_filename(f, 'commonalities.json'))
-    return commonalities
+        subjects[index] = subject
+        write(subjects, get_filename(f, 'commonalities.json'))
+    return subjects 
 
 @targeter
 def sort(targets):
     return [sort_file(t) for t in targets]
 
 def read_sort_file(f):
-    commonalities = []
+    commonalities = dict()
     with open(get_filename(f + ".commonalities", 'json'), 'r') as fp:
         commonalities = json.load(fp)
     
     requests = read_csv(f)
 
     for subject in commonalities.keys():
-        print("\n\n***Enhancement Category: {}***\n".format(subject))
-        
+        print("\n\n***Enhancement Category: {}***".format(subject))
         for category in commonalities[subject].keys():
             print("\n**Subject Category: {}**".format(category))
-            for request in commonalities[subject][category]:
-                print(requests[request], "\n")
+            print("*{}*".format(commonalities[subject][category]['description']))
+            for request in commonalities[subject][category]['requests']:
+                print(requests[request]['Description'])
+                print(requests[request]['Additional Information'], '\n')
 
 
 @targeter
